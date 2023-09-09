e36f122a49ed60dafc88c020c97bf510326d536d
Omkar Chandorkar <gotenksIN@aospa.co>
2023-09-09 15:55:03 +0900

    Introduce Dolby Atmos support
    Change-Id: I46b62d08ad51c321c997792301524fc0c18d6ba6
    Co-authored-by: Sugakesshaa <sugakesshaa@gmail.com>
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>
    Signed-off-by: basamaryan <basam.aryan@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,3 +5,8 @@
 
 # Inherit from the proprietary version
 include vendor/oneplus/dolby/BoardConfigVendor.mk
+
+DOLBY_PATH := device/oneplus/dolby
+
+# Properties
+TARGET_ODM_PROP += $(DOLBY_PATH)/odm.prop
