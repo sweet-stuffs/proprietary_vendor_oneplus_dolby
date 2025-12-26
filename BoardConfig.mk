8cb4d1df32531faac600ca21b9d8cfbd750546de
basamaryan <basam.aryan@gmail.com>
2025-12-26 12:14:07 +0100

    Initial skeleton
    Change-Id: I4c1c129d5b496149b3632bc12990f0b1e6de9c72

---
+++ BoardConfig.mk
@@ -0,0 +1,7 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from the proprietary version
+include vendor/oneplus/dolby/BoardConfigVendor.mk
---
+++ device.mk
@@ -0,0 +1,7 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from the proprietary version
+$(call inherit-product, vendor/oneplus/dolby/dolby-vendor.mk)
