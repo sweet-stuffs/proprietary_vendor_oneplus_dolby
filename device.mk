f9d59b18630b40dd14ec292b860e3f19fa78d453
basamaryan <basam.aryan@gmail.com>
2026-01-01 22:41:42 +0100

    Move dax_default.xml out of proprietary files
    This lets devices copy their own multimedia_dolby_dax_default.xml
    in case they make changes to it
    
    Change-Id: I424767625172e80018ff2aaa2214977b2f155cf8

--- device.mk
+++ device.mk
@@ -6,6 +6,15 @@
 # Inherit from the proprietary version
 $(call inherit-product, vendor/oneplus/dolby/dolby-vendor.mk)
 
+# Configs
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media/media_codecs_c2_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2_dolby_audio.xml
+
+ifneq ($(TARGET_USES_CUSTOM_DOLBY_XML),true)
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/dolby/multimedia_dolby_dax_default.xml:$(TARGET_COPY_OUT_ODM)/etc/dolby/multimedia_dolby_dax_default.xml
+endif
+
 # DolbyManager
 PRODUCT_PACKAGES += \
     OplusDolby
@@ -17,7 +26,3 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
-
-# Media
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media/media_codecs_c2_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2_dolby_audio.xml
