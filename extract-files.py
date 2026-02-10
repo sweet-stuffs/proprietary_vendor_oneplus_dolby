104408fd6fa1ae0ac0422db039ccc05df1bee939
K R I S H N A <krishnakripa34567@gmail.com>
2026-02-10 09:27:47 +0000

    madness
    Change-Id: Ifd24ec45bdb8582a2812e590c454e3a9f95a9367

--- extract-files.py
+++ extract-files.py
@@ -15,6 +15,27 @@ from extract_utils.fixups_blob import (
 )
 
 blob_fixups: blob_fixups_user_type = {
+    'odm/bin/hw/vendor.dolby_sp.media.c2@1.0-service': blob_fixup()
+        .replace_needed('libcodec2_hidl@1.0.so', 'libcodec2_hidl@1.0_sp.so')
+        .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk_sp.so'),
+    'odm/lib64/libcodec2_store_dolby_sp.so': blob_fixup()
+        .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk_sp.so'),
+    ('odm/lib64/libcodec2_soft_ac4dec_sp.so', 'odm/lib64/libcodec2_soft_ddpdec_sp.so'): blob_fixup()
+        .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk_sp.so')
+        .replace_needed('libcodec2_soft_common.so', 'libcodec2_soft_common_sp.so')
+        .replace_needed('libstagefright_foundation.so', 'libstagefright_foundation-v33.so'),
+    ('odm/lib64/libcodec2_soft_common_sp.so', 'odm/lib64/libcodec2_hidl_plugin_sp.so'): blob_fixup()
+        .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk_sp.so')
+        .replace_needed('libstagefright_foundation.so', 'libstagefright_foundation-v33.so'),
+    'odm/lib64/libcodec2_vndk_sp.so': blob_fixup()
+        .replace_needed('libui.so', 'libui_sp.so')
+        .replace_needed('libstagefright_foundation.so', 'libstagefright_foundation-v33.so'),
+    'odm/lib64/libcodec2_hidl@1.0_sp.so': blob_fixup()
+        .replace_needed('libcodec2_hidl_plugin.so', 'libcodec2_hidl_plugin_sp.so')
+        .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk_sp.so'),
+    'odm/lib64/libui_sp.so': blob_fixup()
+        .replace_needed('android.hardware.graphics.common-V3-ndk.so', 'android.hardware.graphics.common-V7-ndk.so')
+        .replace_needed('android.hardware.graphics.allocator-V1-ndk.so', 'android.hardware.graphics.allocator-V2-ndk.so'),
     'odm/etc/vintf/manifest/vendor.dolby.media.c2@1.0-service.xml': blob_fixup()
         .regex_replace('            <instance>dolby</instance>\n', '')
         .regex_replace('            <instance>oplussoftware</instance>\n', '')
